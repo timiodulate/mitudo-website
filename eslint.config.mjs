cac2fd9fe7e3456f0981d0e8a2a07a1293c9f26a
Timileyin Odulate <timiodulate@gmail.com>
2025-06-05 13:08:10 +0100

    tinkers with eslig configfor deployment sake

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
 			"react/no-unescaped-entities": "off",
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/no-empty-object-type": "off",
+			semi: "off",
 		},
 	},
 ];
