50f5c3f3b31e50d4d3ad0c6786ee13f17d488c7f
Timileyin Odulate <timiodulate@gmail.com>
2025-06-05 23:37:06 +0100

    fix

--- src/components/ui/Container.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { containerRecipe } from "@/styles/components/container.recipe";
-import { chakra } from "@chakra-ui/react";
-
-export const Container = chakra("container", containerRecipe);
