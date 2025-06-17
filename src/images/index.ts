fb6d346c7cc923a870bc18de98816ed9fdcbb30b
Timileyin Odulate <timiodulate@gmail.com>
2025-06-17 16:18:47 +0100

    adds a placeholder image for projects

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,3 +2,8 @@ import heroIllustration from "./hero-ill.png";
 import webDevIllustration from "./web-dev-ill.png";
 
 export { heroIllustration, webDevIllustration };
+
+// projects
+import ecommBanner from "./ecom.jpg";
+
+export { ecommBanner };
