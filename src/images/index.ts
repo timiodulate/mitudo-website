afea647a78a08f48f07307f8158d468d5c94b916
Timileyin Odulate <timiodulate@gmail.com>
2025-06-19 16:24:09 +0100

    adds the services icons

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,14 @@
 import heroIllustration from "./hero-ill.png";
-import webDevIllustration from "./web-dev-ill.png";
 
-export { heroIllustration, webDevIllustration };
+export { heroIllustration };
+
+// services
+// import webDevIllustration from "./web-dev-ill.png";
+import webDevIllustration from "./web-dev-ill.svg";
+import mobileDevIllustration from "./mobile-dev-ill.svg";
+import softwareIllustration from "./software-ill.svg";
+
+export { webDevIllustration, mobileDevIllustration, softwareIllustration };
 
 // projects
 import ecommBanner from "./ecom.jpg";
